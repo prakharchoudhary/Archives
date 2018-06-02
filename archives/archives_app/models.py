3a0e77f5a75d0cbd3a4bb7c90a88f13304eb9039
prakharchoudhary <prakhar2397@gmail.com>
2018-06-02 23:48:54 +0530

    add path field to Category model

--- archives/archives_app/models.py
+++ archives/archives_app/models.py
@@ -8,6 +8,7 @@ class Category(models.Model):
 	"""
 
 	name = models.CharField(max_length=200, help_text="Enter a category name")
+	path = models.CharField(max_length=256, help_text="Enter the path of file on local system.")
 	subdirs = models.ForeignKey('Category', on_delete=models.CASCADE, null=True, blank=True)
 
 	class Meta:
