09122ddc00ace0dc1261d02bc5e91805179016b3
prakharchoudhary <prakhar2397@gmail.com>
2018-06-06 11:06:26 +0530

    DBwriter ready and functional

--- archives/archives_app/dbWriter.py
+++ archives/archives_app/dbWriter.py
@@ -11,18 +11,17 @@ such that only the new ones are appended in the database.
 import os
 from .models import Category, Book
 from pprint import pprint
+from django.db.utils import IntegrityError
 
-class Traverse:
+class DBwriter:
 
 	roots = []
 	directories = []
 	books = []
 
-	def __init__(self, cursor, dir_path, category_table, books_table):
+	def __init__(self, cursor, dir_path):
 		self.cur = cursor
 		self.path = dir_path
-		self.category_table = category_table
-		self.books_table = books_table
 
 	def get_all_items(self):
 		'''
@@ -44,10 +43,42 @@ class Traverse:
 		for root in self.roots:
 			cat = Category(name=root[1], path=root[0])
 			cat.save()
+		self._update_relations()	# this call updates the parent child 
+									# relation among dirs.
 
 	def _update_relations(self):
 		'''
 		Use the directories list to update the parent-child relations between
 		categories
 		'''
-		pass 
+		for dirr in self.directories:
+			parent = Category.objects.get(name=dirr[0])
+			child = Category.objects.get(name=dirr[1])
+			parent.category_set.add(child)
+
+	def gen_books(self):
+		'''
+		Use the books list to make entries and assign parent directories.
+		'''
+		books = Book.objects.all()
+		allBooks = [b.title for b in books]
+		for bookIn in self.books:
+			try:
+				cat = Category.objects.get(name=bookIn[0])
+			except Exception as e:
+				print(e)
+			bpath = os.path.join(cat.path, bookIn[1])
+			try:
+				if bookIn[1] not in allBooks:
+					cat.book_set.create(title=bookIn[1],path=bpath)
+			except IntegrityError:	# incase another book with same name exists
+				continue
+
+	def populate_db(self):
+		'''
+		Main function that populates the database.
+		'''
+		self.get_all_items()
+		self.gen_categories()
+		self.gen_books()
+
