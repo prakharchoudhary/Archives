69dd72518bb60198abfd2ec8cebbd6923d654c0a
prakharchoudhary <prakhar2397@gmail.com>
2018-06-08 17:42:16 +0530

    added filters and search bar to admin

--- archives/archives_app/admin.py
+++ archives/archives_app/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 from .models import Category, Book
 # Register your models here.
 
-admin.site.register(Category)
-admin.site.register(Book)
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = ('name', 'path', 'parent')
+	list_filter = ('parent',)
+	search_fields = ('name', 'parent')
+
+class BookAdmin(admin.ModelAdmin):
+	list_display = ('title', 'category', 'status')
+	list_filter = ('category', 'status')
+	list_editable = ('status',)
+	search_fields = ('title', 'category__name', 'status')
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Book, BookAdmin)
